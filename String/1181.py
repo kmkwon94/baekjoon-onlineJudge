32112ffbd9d7728b5890242eac875c73bc04fcdd
kmkwon94 <kmkwon94@kookmin.ac.kr>
2021-03-06 18:56:15 +0900

    upload 1181.py

---
+++ String/1181.py
@@ -0,0 +1,12 @@
+
+loop = int(input())
+string_list = []
+while loop > 0:
+    string = input()
+    string_list.append(string)
+    loop -= 1
+string_list = list(set(string_list))
+answer = (sorted(string_list, key=lambda x: (len(x), x)))
+
+for string in answer:
+    print(string)
