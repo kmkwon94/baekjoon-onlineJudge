bd4843142c49c525f817ad952b1b86171d5e8a8c
kmkwon94 <kmkwon94@kookmin.ac.kr>
2021-04-28 03:48:09 +0900

    fix : 2583.py

--- graph/2583.py
+++ graph/2583.py
@@ -20,20 +20,23 @@ sys.setrecursionlimit(10**7)
 
 def dfs(x,y):
     global cnt
+    # 좌표 바깥으로 나간 값들은 return False
     if x < 0 or x >= n or y <0 or y >= m:
         return False,cnt
-
+    
+    # 직사각형이 존재하는 부분은 return False
     if matrix[x][y] == 1:
         return False,cnt
     
+    # 방문하지 않았으면
     if visited[x][y] == 0:
-        visited[x][y] = 1
-        cnt += 1
+        visited[x][y] = 1 # 방문처리 후 dfs
+        cnt += 1 # 직사각형의 갯수가 곧 넓이이므로 카운트 세줌
         dfs(x-1,y)
         dfs(x+1,y)
         dfs(x,y-1)
         dfs(x,y+1)
-        return True,cnt
+        return True,cnt 
     return False,cnt
 
 m,n,k = map(int, sys.stdin.readline().split())
@@ -53,11 +56,11 @@ for i in range(n):
     for j in range(m):
         cnt = 0
         tf, size = dfs(i,j)
-        if tf == True:
-            tf_count += 1
-            size_list.append(size)
-size_list.sort()
-print(tf_count)
+        if tf == True: # dfs가 온전히 끝났으면
+            tf_count += 1 # 끝난 횟수를 센다. 즉 영역의 갯수
+            size_list.append(size) #넓이는 size_list에 추가
+size_list.sort() #오름차순 정렬
+print(tf_count) 
 
 for i in size_list:
     print(i, end= ' ')
